ad4de7f184f7b7de8f712308c38b7d0dbf28a0b7
dantemessy <thepunishar1@gmail.com>
2020-10-10 19:24:45 +0300

    working like a charm

--- src/components/mainCounter.js
+++ src/components/mainCounter.js
@@ -5,23 +5,59 @@ import { counterContext } from "../contexts/counterContext.js";
 function MainCounter() {
 
     let counterData = useContext(counterContext);
+    console.log("MainCounter -> counterData", counterData)
     const [screen, setScreen] = useState(counterData.profiles.map((profile, idx) => {
-        return <li key={idx} onClick={e => handleClick(profile.data)}> {profile.name}</li>
+        return <li key={idx} onClick={e => handleClick(profile)}> {profile.name}</li>
     }));
 
+    let interval, isPaused;
 
-    let handleClick = (data) => {
-        setScreen(currentProfile(data));
+    let handleClick = (profile) => {
+        setScreen(currentProfile(profile));
     }
 
-    let currentProfile = (profile) => profile.map((counter, idx) => {
-        return <li key={idx}> {counter.time} X {counter.repeat} </li>
-    })
+    let currentProfile = (profile) =>
+        (counterData.setCurrentProfile(profile),
+            profile.data.map((counter, idx) => {
+                return <li key={idx}> {counter.time} X {counter.repeat} </li>
+            }))
 
+    let countDown = time => new Promise((rs, rj) =>
+        interval = setInterval(() => {
+            if (isPaused) {
+                return rj('Paused');
+            }
+            counterData.setTime(time);
+            if (time <= 0) {
+                clearInterval(interval);
+                rs();
+            }
+            console.log(time);
+            time--;
+        }, 1000));
+
+
+    let run = async(countDownData) => {
+
+            await countDown(countDownData.time);
+            countDownData.repeat--
+            if (countDownData.repeat !== 0) run(countDownData);
+            return;
+    }
+
+    let main = async () => {
+
+        counterData.currentProfile.data.forEach(element => {
+            run(element)
+        })
+
+    }
 
     return (
         <div>
+            <div>{counterData.time}</div>
             {screen}
+            <button onClick={e => main()}> START</button>
         </div>
     );
 }
--- src/contexts/counterContext.js
+++ src/contexts/counterContext.js
@@ -6,9 +6,10 @@ export const counterContext = React.createContext();
 function CounterProvider(props) {
 
     const [time, setTime] = useState(0);
-    const [profiles, setProfiles] = useState([{name: 'default', data: [{repeat: 3, time: 5}]}]);
+    const [profiles, setProfiles] = useState([{ name: 'default', data: [{ repeat: 3, time: 5 }] }]);
+    const [currentProfile, setCurrentProfile] = useState(profiles[0]);
 
-    let keys = {time, setTime, profiles};
+    let keys = { time, setTime, profiles, setProfiles, currentProfile, setCurrentProfile};
 
     return (
         <counterContext.Provider value={keys}>
